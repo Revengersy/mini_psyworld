6bf4cc73ba43ef60ef2a28ee58b8a9f88b65074e
KyeongranMun <eggking2580@gmail.com>
2024-11-06 17:24:17 +0900

    Revert "리팩터링(firebase모듈화, 코드중복 제거)"

--- firebase/config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBpsgZrksta0aLlWNG01534vA1s45rUXzs",
-  authDomain: "minipsyworld.firebaseapp.com",
-  projectId: "minipsyworld",
-  storageBucket: "minipsyworld.firebasestorage.app",
-  messagingSenderId: "69424392710",
-  appId: "1:69424392710:web:5e3b7e1ccfe20c62a5b187",
-  measurementId: "G-F8ZR9BZQJQ"
-};
